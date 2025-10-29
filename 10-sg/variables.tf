f3b2855a50f8d6df79f47ebba30e908b47dbbd42
Deepthi Kattika <deepthi.kattika@gmail.com>
2025-10-29 09:27:35 +0800

    mongodb

--- 10-sg/variables.tf
+++ 10-sg/variables.tf
@@ -5,3 +5,13 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "sg_names" {
+    default = ["mongodb","redis","mysql","rabbitmq",
+                "catalogue","user","cart","shipping","payment",
+                "frontend",
+                "bastion",
+                "frontend_alb",
+                "backend_alb"]
+}
+
---
+++ 30-sg-rules/main.tf
@@ -0,0 +1,30 @@
+# # frontend accepting traffic from frontend LB.
+# resource "aws_security_group_rule" "frontned_frontend_lb" {
+#   type                      = "ingress"
+#   security_group_id         = module.sg[9].sg_id # forntend SG_ID
+#   source_security_group_id  = module.sg[11].sg_id # frontend LB SG_ID
+#   from_port                 = 80
+#   protocol                  = "tcp"
+#   to_port                   = 80
+# }
+
+
+# backend accepting traffic from bastion.
+resource "aws_security_group_rule" "backend_alb_bastion" {
+  type                      = "ingress"
+  security_group_id         = local.backend_alb_sg_id# forntend SG_ID
+  source_security_group_id  = local.bastion_sg_id# frontend LB SG_ID
+  from_port                 = 80
+  protocol                  = "tcp"
+  to_port                   = 80
+}
+
+
+resource "aws_security_group_rule" "bastion_laptop" {
+  type                      = "ingress"
+  security_group_id         = local.bastion_sg_id  # forntend SG_ID
+  cidr_blocks               = ["0.0.0.0/0"]# frontend LB SG_ID
+  from_port                 = 22
+  protocol                  = "tcp"
+  to_port                   = 22
+}
