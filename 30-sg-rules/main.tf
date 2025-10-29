a9b8fb67d6b5a1fb6173c072708c627f375ed990
Deepthi Kattika <deepthi.kattika@gmail.com>
2025-10-29 15:33:14 +0800

    redis

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -31,7 +31,7 @@ resource "aws_security_group_rule" "bastion_laptop" {
 
 resource "aws_security_group_rule" "mongodb_bastion" {
   type                      = "ingress"
-  security_group_id         = local.mongodb_sg_id  # forntend SG_ID
+  security_group_id         = local.mongodb_sg_id  
   source_security_group_id  = local.bastion_sg_id
   from_port                 = 22
   protocol                  = "tcp"
@@ -40,7 +40,16 @@ resource "aws_security_group_rule" "mongodb_bastion" {
 
 resource "aws_security_group_rule" "redis_bastion" {
   type                      = "ingress"
-  security_group_id         = local.redis_sg_id  # forntend SG_ID
+  security_group_id         = local.redis_sg_id  
+  source_security_group_id  = local.bastion_sg_id
+  from_port                 = 22
+  protocol                  = "tcp"
+  to_port                   = 22
+}
+
+resource "aws_security_group_rule" "rabbitmq_bastion" {
+  type                      = "ingress"
+  security_group_id         = local.rabbitmq_sg_id 
   source_security_group_id  = local.bastion_sg_id
   from_port                 = 22
   protocol                  = "tcp"
