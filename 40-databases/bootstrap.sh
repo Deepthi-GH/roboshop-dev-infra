5801729eae7e80528fa13ce0c670c7c9a511ff3f
Deepthi Kattika <deepthi.kattika@gmail.com>
2025-10-31 00:01:04 +0800

    redis

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -22,5 +22,5 @@ else
     cd $ANSIBLE_DIR 
 fi
 
-ansible-playbook -e component=$component environment=$environment main.yml
+ansible-playbook -e component=$component -e environment=$environment main.yml
 
