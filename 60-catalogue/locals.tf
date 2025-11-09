549566a0a453d13ca4a8930138d9db3bbbda2c82
Deepthi Kattika <deepthi.kattika@gmail.com>
2025-11-10 00:08:54 +0800

    user

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -3,6 +3,7 @@ locals {
     common_name_suffix  = "${var.project_name}-${var.environment}" #roboshop-dev
     vpc_id              = data.aws_ssm_parameter.vpc_id.value
     catalogue_sg_id     = data.aws_ssm_parameter.catalogue_sg_id.value
+    user_sg_id          = data.aws_ssm_parameter.user_sg_id.value
     private_subnet_id   = split("," ,data.aws_ssm_parameter.private_subnet_ids.value)[0]
     private_subnet_ids  = split("," ,data.aws_ssm_parameter.private_subnet_ids.value)
     backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -210,3 +210,225 @@ provisioner "local-exec" {
     command = "aws ec2 terminate-instances --instance-ids ${aws_instance.catalogue.id}"
 }
 }
+
+
+resource "aws_instance" "user" {
+  ami           = local.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [local.user_sg_id]
+  subnet_id     = local.private_subnet_id
+
+ tags = merge (
+    local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-user" #roboshop-dev-user
+    }
+  )
+}
+
+# connect to instance using remote-exec provisioner through terraform data
+
+resource "terraform_data" "user" {
+    depends_on = [terraform_data.catalogue]
+    triggers_replace = [
+        aws_instance.user.id # triggers when user id changes
+    ]
+
+connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.user.private_ip
+}
+
+# terraform copies the file to user server
+
+provisioner "file" {
+    source      = "user.sh"
+    destination = "/tmp/user.sh"
+}
+
+provisioner "remote-exec" {
+    inline = [
+        " chmod +x /tmp/user.sh ",
+        "sudo sh /tmp/user.sh user ${var.environment}" 
+    
+    ]
+}
+}
+
+resource "aws_route53_record" "user" {
+  zone_id = var.zone_id
+  name    = "user-${var.environment}.${var.domain_name}" # user-dev.deepthi.cloud
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.user.private_ip]
+  allow_overwrite = true
+}
+
+# stop the instance
+resource "aws_ec2_instance_state" "user" {
+  instance_id = aws_instance.user.id
+  state       = "stopped"
+  depends_on  = [terraform_data.user]
+}
+
+# Take AMI
+resource "aws_ami_from_instance" "user" {
+  name               = "${local.common_name_suffix}-user"
+  source_instance_id = aws_instance.user.id
+  depends_on         = [aws_ec2_instance_state.user]
+
+   tags = merge (
+    local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-user" #roboshop-dev-user
+    }
+  )
+}
+
+# create target group
+resource "aws_lb_target_group" "user" {
+  name     = "${local.common_name_suffix}-user"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = local.vpc_id # Replace with your VPC ID
+  deregistration_delay = 60 # waiting period before deleting the instance.
+  health_check {
+    path                = "/health"
+    protocol            = "HTTP"
+    port                = 8080
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 2
+    interval            = 10
+    matcher             = "200-299"
+  }
+
+    tags = merge (
+    local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-user" #roboshop-dev-user
+    }
+  )
+}
+
+# Launch Template
+resource "aws_launch_template" "user" {
+  name = "${local.common_name_suffix}-user" 
+  image_id = aws_ami_from_instance.user.id
+  instance_initiated_shutdown_behavior = "terminate"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.user_sg_id]
+
+  tag_specifications {
+    resource_type = "instance"
+
+      tags = merge (
+    local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-user" #roboshop-dev-user
+    }
+  )
+  }
+
+   # tags attached to the volume created by instance
+    tag_specifications {
+    resource_type = "volume"
+
+    tags = merge(
+      local.common_tags,
+     {
+      Name = "${local.common_name_suffix}-user"
+    }
+    )
+  }
+
+ # tags attached to the launch template 
+   tags = merge(
+      local.common_tags,
+     {
+      Name = "${local.common_name_suffix}-user"
+    }
+    )
+
+}
+
+# give launch template to auto scaling.
+
+# AutoScaling group
+resource "aws_autoscaling_group" "user" {
+  name                      = "${local.common_name_suffix}-user"
+  max_size                  = 10
+  min_size                  = 1
+  health_check_grace_period = 100
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+  force_delete              = false
+  launch_template {
+    id      = aws_launch_template.user.id
+    version = aws_launch_template.user.latest_version
+  }
+  vpc_zone_identifier       = local.private_subnet_ids
+  target_group_arns         = [aws_lb_target_group.user.arn]
+
+  dynamic "tag" { # we will get the iterator with the name as tag
+    for_each = merge(
+      local.common_tags,
+      {
+        Name = "${local.common_name_suffix}-user"
+      }
+    )
+    content {
+    key                 = tag.key
+    value               = tag.value
+    propagate_at_launch = true
+  }
+  }
+  timeouts {
+    delete = "15m" # launch in 15m,otherwise timeout.
+  }
+}
+
+# autoscaling policy
+resource "aws_autoscaling_policy" "user" {
+  # ... other configuration ...
+  autoscaling_group_name = aws_autoscaling_group.user.name
+  name                   = "${local.common_name_suffix}-user"
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 75.0
+  }
+}
+
+# create LB Listener rule.
+resource "aws_lb_listener_rule" "user" {
+  listener_arn = local.backend_alb_listener_arn
+  priority     = 11
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.user.arn
+  }
+
+  condition {
+    host_header {
+      values = ["user.backend-alb-${var.environment}.${var.domain_name}"]
+    }
+  }
+}
+
+resource "terraform_data" "user_local" {
+    triggers_replace = [
+        aws_instance.user.id # triggers when user id changes
+    ]
+depends_on = [ aws_autoscaling_policy.user ]
+provisioner "local-exec" {
+    command = "aws ec2 terminate-instances --instance-ids ${aws_instance.user.id}"
+}
+}
